304713945766860daa577289305de039faf37418
GitHub Action <action@github.com>
2022-11-14 06:52:31 +0000

    Template cleanup

--- src/main/kotlin/com/github/iktsuarpokluo/oneliner/MyBundle.kt
+++ src/main/kotlin/com/github/iktsuarpokluo/oneliner/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.iktsuarpokluo.oneliner
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/iktsuarpokluo/oneliner/services/MyProjectService.kt
+++ src/main/kotlin/com/github/iktsuarpokluo/oneliner/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.iktsuarpokluo.oneliner.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.iktsuarpokluo.oneliner.MyBundle
 
 class MyProjectService(project: Project) {
 
